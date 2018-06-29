05d0616e64be0594c7b5b54881e102de0473ad7a
Sharmila <ppsharmi@gmail.com>
2018-06-28 17:18:30 -0700

    added element in array

--- array-last.js
+++ array-last.js
@@ -1,6 +1,6 @@
 // find last element of the array
 
-a = ['abc','family','badminton','san jose']
+a = ['123','abc','family','badminton','san jose']
 function last(a){
   var n = a.length;
   var b = a[n-1];
